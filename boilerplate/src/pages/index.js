d26e3e8b98503fe647b897d4f331b76bebf85191
xiaohuoni <chen.xiaocong2@iwhalecloud.com>
2022-05-30 00:19:04 +0800

    feat: d5 http://juejin.cn/post/7103188188105015333

--- boilerplate/src/pages/index.js
+++ boilerplate/src/pages/index.js
@@ -1,1 +1,17 @@
-export default () => <div>Index Page</div>;
+import { Link, useSearchParams, createSearchParams, useLocation } from 'umi';
+
+export default () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const location = useLocation();
+    console.log(location)
+    const a = searchParams.get('a');
+    const b = searchParams.get('b');
+    return <div>Index Page
+        <p><Link to="/user">Go to user page</Link></p>
+        <p>SearchParams ---- a:{a};b:{b}</p>
+        <p>State ---- {JSON.stringify(location?.state)}</p>
+        <button onClick={() => {
+            setSearchParams(createSearchParams({ a: 123, b: 456 }));
+        }}>Change SearchParams</button>
+    </div>
+};
--- boilerplate/src/pages/user.js
+++ boilerplate/src/pages/user.js
@@ -1,1 +1,24 @@
-export default () => <div>User Page</div>;
+import { history, useNavigate, createSearchParams } from 'umi';
+
+export default function User() {
+    const navigate = useNavigate();
+    return (
+        <div>
+            <h1>User Page</h1>
+            <button onClick={() => history.back()}>go back by history!</button>
+            <button onClick={() => history.push('/')}>go to index by history!</button>
+            <button onClick={() => navigate(-1)}>go back by navigate!</button>
+            <button onClick={() => navigate('/')}>go to index by navigate!</button>
+            <button onClick={() => {
+                navigate(`/?${createSearchParams({ a: 1, b: 2 })}`)
+            }}>go to index has SearchParams!</button>
+            <button onClick={() => {
+                navigate('/', {
+                    state: {
+                        c: 987
+                    }
+                })
+            }}>go to index has State!</button>
+        </div>
+    );
+}
